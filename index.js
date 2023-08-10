c72e60c2beec5f10f93d44bbe49dd4bf64dc0b86
dev-ys-36 <tkddbs7631@naver.com>
2023-08-10 16:54:32 +0900

    updated syu kr list

--- index.js
+++ index.js
@@ -27,4 +27,4 @@ app.get('*', (req, res) => {
   res.status(404).json({statusCode: 404, message: 'unknown request.'})
 })
 
-http.createServer(app).listen(1233, '0.0.0.0')
+http.createServer(app).listen(4444, '0.0.0.0')
